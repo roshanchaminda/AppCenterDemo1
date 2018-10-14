2406e9affaff5ca9f482b30ce3168d40a2440425
roshan <roshanchaminda84@gmail.com>
2018-10-14 14:48:36 +0530

    * App.xaml.cs: Initiate App analytics and crash feature
    * AppCenterDemo1.csproj:

--- AppCenterDemo1/App.xaml.cs
+++ AppCenterDemo1/App.xaml.cs
@@ -1,6 +1,9 @@
 using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
 namespace AppCenterDemo1
@@ -17,6 +20,10 @@ namespace AppCenterDemo1
         protected override void OnStart()
         {
             // Handle when your app starts
+            AppCenter.Start("ios=e5d04d89-fa13-4813-b021-47e8a6dac3da;" +
+                  "uwp={Your UWP App secret here};" +
+                  "android={Your Android App secret here}",
+                            typeof(Analytics), typeof(Crashes));
         }
 
         protected override void OnSleep()
